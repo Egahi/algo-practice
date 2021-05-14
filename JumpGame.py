a291d9e56eb1569737ddf696453e726169b4faa2
Egahi S. O <okworisamuel6@gmail.com>
2021-05-14 09:50:42 +0100

    Jump game - dp

--- JumpGame.py
+++ JumpGame.py
@@ -5,18 +5,22 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        memo = ['U']*len(nums)
         def canJumpFromPosition(position, nums):
-            if position == len(nums) - 1:
-                return True
+            if memo[position] != 'U':
+                return memo[position] == 'G'
 
             furthestJump = min(position + nums[position], len(nums) - 1)
             
-            for nextPosition in range(position + 1, furthestJump + 1):
+            for nextPosition in reversed(range(position + 1, furthestJump + 1)):
                 if canJumpFromPosition(nextPosition, nums):
+                    memo[position] = 'G'
                     return True
 
+            memo[position] = 'B'
             return False
-            
+
+        memo[len(nums) - 1] = 'G'
         return  canJumpFromPosition(0, nums)
 
 nums = [2,3,1,1,4]
