5a899c933101ca602d6b6176cae2dd4f2f575bbd
Egahi S. O <okworisamuel6@gmail.com>
2021-05-14 10:19:15 +0100

    Jump game - Greedy

--- JumpGame.py
+++ JumpGame.py
@@ -5,20 +5,16 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        memo = ['U']*(len(nums) - 1)
-        memo.append('G')
+        lastPos = len(nums) - 1
 
-        for i in range(len(nums) - 2, -1, -1):
-            furthestJump = min(i + nums[i], len(nums) - 1)
-            for j in range(i + 1, furthestJump + 1):
-                if memo[j] == 'G':
-                    memo[i] = 'G'
-                    break
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= lastPos:
+                lastPos = i
 
-        return  memo[0] == 'G'
+        return  lastPos == 0
 
 nums = [2,3,1,1,4]
-nums = [1,2]
-nums = [0,2,3]
+#nums = [1,2]
+#nums = [0,2,3]
 obj = Solution()
 print(obj.canJump(nums))
