f014556e7f521bc30474e6f41d3912cad4e7f155
Egahi S. O <okworisamuel6@gmail.com>
2021-05-18 09:51:51 +0100

    Permutations

---
+++ Permutations.py
@@ -0,0 +1,24 @@
+'''
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/795/
+'''
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first):
+            if first == n:
+                output.append(nums[:])
+
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(i + 1)
+                nums[first], nums[i] = nums[i], nums[first]
+
+        output = []
+        n = len(nums)
+        backtrack(0)
+        return output
+
+nums = [1,2,3]
+obj = Solution()
+print(obj.permute(nums))
